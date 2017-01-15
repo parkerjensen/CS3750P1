704e1e74a51977fed295d66fcd5f779cc010689b
Parker <parker.jensen@aggiemail.usu.edu>
2017-01-15 10:57:42 -0700

    Initial Commit

---
+++ CS3750P1/CS3750P1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CS3750P1.Startup))]
+namespace CS3750P1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
